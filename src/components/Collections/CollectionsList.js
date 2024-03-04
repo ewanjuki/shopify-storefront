7d2567b54f50068660ab9495692e2a160a62866f
Edwin Wanjuki <edwinwanjuki@gmail.com>
2024-03-04 14:20:48 +0300

    filter collections based on product image

--- src/components/Collections/CollectionsList.js
+++ src/components/Collections/CollectionsList.js
@@ -4,28 +4,32 @@ import CollectionItem from "./CollectionItem";
 import BrowseAllProducts from "../UI/BrowseAllProducts";
 
 function CollectionsList(props) {
-  const mappedCollections = props.collections.map((collection) => {
-    let imageSrc;
-    if (!collection.node.image) {
-      imageSrc =
-        collection.node.products.edges[0].node.images.edges[0].node
-          .transformedSrc;
-    } else {
-      imageSrc = collection.node.image.transformedSrc;
-    }
+  const mappedCollections = props.collections
+    .filter((collection) => {
+      return collection.node.products.edges[0].node.images.edges.length > 0;
+    })
+    .map((collection) => {
+      let imageSrc;
+      if (!collection.node.image) {
+        imageSrc =
+          collection.node.products.edges[0].node.images.edges[0].node
+            .transformedSrc;
+      } else {
+        imageSrc = collection.node.image.transformedSrc;
+      }
 
-    return (
-      <CollectionItem
-        key={collection.node.handle}
-        collection={{
-          handle: collection.node.handle,
-          title: collection.node.title,
-          altText: collection.node.handle,
-          imageSrc,
-        }}
-      />
-    );
-  });
+      return (
+        <CollectionItem
+          key={collection.node.handle}
+          collection={{
+            handle: collection.node.handle,
+            title: collection.node.title,
+            altText: collection.node.handle,
+            imageSrc,
+          }}
+        />
+      );
+    });
 
   return (
     <div className={classes["collections-list"]}>
